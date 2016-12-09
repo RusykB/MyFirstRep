44839ab50be452ec299cf0a5a31d4a07cc82b642
RusykB <rusbir79@yandex.ru>
2016-12-09 15:04:32 +0300

    20161209

---
+++ level08/lesson11/home05/Solution.java
@@ -0,0 +1,58 @@
+package com.javarush.test.level08.lesson11.home05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/* Мама Мыла Раму. Теперь с большой буквы
+Написать программу, которая вводит с клавиатуры строку текста.
+Программа заменяет в тексте первые буквы всех слов на заглавные.
+Вывести результат на экран.
+
+Пример ввода:
+  мама     мыла раму.
+
+Пример вывода:
+  Мама     Мыла Раму.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
+        List<String> list = new ArrayList<String>();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+
+        //Pattern p = Pattern.compile("^[a-z0-9_-]{3,15}$");
+        //Matcher m = p.matcher(userNameString);
+        //s.replaceAll("^[а-я]","x");
+
+        char[] tmpChr;
+        String tmpStr;
+        char ch;
+        for (String tmp:s.split(" * "))
+        {
+            //System.out.println(tmp);
+            tmpChr=tmp.toCharArray();
+
+            tmpStr=""+tmpChr[0];
+            tmpStr=tmpStr.toUpperCase();
+            System.out.println(tmpStr);
+            tmpStr=tmp.replaceFirst(" ",tmpStr);
+            System.out.println(tmpStr);
+            //tmp.toUpperCase(); //tmpChr[0]
+            //list.add(tmp.toCharArray().[0]);
+
+        }
+
+        System.out.println(list);
+
+    }
+
+
+}

ce9079e0417b9742c7958c80646d6d6d53981b65
RusykB <rusbir79@yandex.ru>
2016-12-12 15:13:43 +0300

    20161212

--- level08/lesson11/bonus02/Solution.java
+++ level08/lesson11/bonus02/Solution.java
@@ -3,8 +3,7 @@ package com.javarush.test.level08.lesson11.bonus02;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 /* Нужно добавить в программу новую функциональность
 Задача: Программа определяет, какая семья (фамилию) живёт в доме с указанным номером.
@@ -30,22 +29,25 @@ public class Solution
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         //list of addresses
-        List<String> addresses = new ArrayList<String>();
+        //List<String> addresses = new ArrayList<String>();
+        HashMap<String,String> addresses = new HashMap<>();
         while (true)
         {
+            String city = reader.readLine();
+            if (city.isEmpty()) break;
             String family = reader.readLine();
-            if (family.isEmpty()) break;
 
-            addresses.add(family);
+
+            addresses.put(city,family);
         }
 
         //read home number
-        int houseNumber = Integer.parseInt(reader.readLine());
+        String city = reader.readLine();
 
-        if (0 <= houseNumber && houseNumber < addresses.size())
-        {
-            String familySecondName = addresses.get(houseNumber);
+        //if (0 <= houseNumber && houseNumber < addresses.size())
+        //{
+            String familySecondName = addresses.get(city);
             System.out.println(familySecondName);
-        }
+        //}
     }
 }
